d98985a514065b8cb9559d1174389faf44bfd89f
anthonyeef <anthonyeef@gmail.com>
2015-03-07 16:58:41 +0800

    last last try!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :microposts
-	validate name, presence: true
-	validate email, presence: true 
+	validate :name, presence: true
+	validate :email, presence: true 
 end
