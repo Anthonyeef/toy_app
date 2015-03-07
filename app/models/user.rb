74938f4ce21e0050238937106f6d04ba6d9bde9e
anthonyeef <anthonyeef@gmail.com>
2015-03-07 17:15:36 +0800

    fix the bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :microposts
-	validate :name, presence: true
-	validate :email, presence: true 
+	validates :user_name, presence: true
+	validates :user_email, presence: true 
 end
